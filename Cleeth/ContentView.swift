275bdec9fac701e2cd36051358b948c4301130b2
Matias Luna <matluna.pub@gmail.com>
2023-02-10 18:35:06 +0000

    Set the first version of the notification push local

--- Cleeth/ContentView.swift
+++ Cleeth/ContentView.swift
@@ -54,6 +54,7 @@ struct ContentView: View {
     
     var body: some View {
         
+        
         ZStack(){
             
             Color.white
@@ -129,6 +130,59 @@ struct ContentView: View {
                 }
                 
                 Spacer()
+                
+                VStack{
+                    HStack {
+                        Button("Request Permission") {
+                            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                                if success {
+                                    print("All set!")
+                                } else if let error = error {
+                                    print(error.localizedDescription)
+                                }
+                            }
+                        }
+                        
+                        Button("Schedule Notification") {
+                            
+                            print("Schedule Notification")
+                            
+                            let content = UNMutableNotificationContent()
+                            content.title = "Time to brush your teeth"
+                            content.subtitle = "Feel fresh for a productive afternoon"
+                            content.sound = UNNotificationSound.default
+
+
+                            // show this notification five seconds from now
+                            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                            
+                            // Configure the recurring date.
+                            var dateComponents = DateComponents()
+                            dateComponents.calendar = Calendar.current
+
+                            dateComponents.hour = 18    // 14:00 hours
+                            dateComponents.minute = 20
+                            dateComponents.second = 20
+                               
+                            // Create the trigger as a repeating event.
+                            //let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+                            // Create the request
+                            let uuidString = UUID().uuidString
+                            let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+
+                            // Schedule the request with the system.
+                            let notificationCenter = UNUserNotificationCenter.current()
+                            notificationCenter.add(request) { (error) in
+                                if error != nil {
+                                    print("Error in Notification")
+                                }
+                            }
+                        }
+                        
+                    }
+                    
+                }
                 
             }
             
