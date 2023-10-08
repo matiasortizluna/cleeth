b5369b62592ccda49f2370867c34362e483a9e36
matiasluna22 <matluna.pub@gmail.com>
2023-10-08 18:48:04 +0100

    BackgroundView: Animation of Emojis

---
+++ Cleeth/BrushView/BackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundView.swift
+//  Cleeth
+//
+//  Created by Matias Ariel Ortiz Luna on 8/10/23.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundView()
+}
---
+++ Cleeth/BrushView/BrushView.swift
@@ -0,0 +1,28 @@
+//
+//  BrushView.swift
+//  Cleeth
+//
+//  Created by Matias Ariel Ortiz Luna on 16/02/2023.
+//
+
+import SwiftUI
+
+struct BrushView: View {
+    var body: some View {
+        
+        ZStack(){
+            
+            BackgroundView()
+            
+            Text("Brush View")
+        }
+        
+        
+        
+        
+    }
+}
+
+#Preview {
+    BrushView()
+}
